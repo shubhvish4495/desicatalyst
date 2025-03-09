00bacaf8ba7e02ac1966702e766d56c3408af630
Shubham Saurav <ssaurav@ssaurav-thinkpadp16vgen1.punetw6.csb>
2025-03-09 09:42:52 +0530

    fix-2

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -5,3 +5,5 @@ cp -r server/data dist/data
 cp attached_assets/logo-fullsize.png dist/public/logo-fullsize.png
 echo "Setup complete, Now Deploying..."
 nohup node dist/index.js &
+echo "Deployment made. Tailing nohup.out"
+tail -f nohup.out
